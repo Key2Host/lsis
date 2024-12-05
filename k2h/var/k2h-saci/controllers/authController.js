802777c973364d55c71c2908bb8336937a894013
lulu <lukaspoggemann@gmail.com>
2024-12-05 21:29:32 +0100

    saci

---
+++ k2h/var/k2h-saci/controllers/authController.js
@@ -0,0 +1,31 @@
+exports.register = (req, res) => {
+
+};
+
+exports.login = (req, res) => {
+
+};
+
+exports.refresh = (req, res) => {
+
+};
+
+exports.logout = (req, res) => {
+
+};
+
+exports.pwreset = (req, res) => {
+
+};
+
+exports.totpsetup = (req, res) => {
+
+};
+
+exports.totpconfirm = (req, res) => {
+
+};
+
+exports.totpremove = (req, res) => {
+
+};
