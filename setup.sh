ae26b2b8060d77cba75798eb2e4c4aca541a42ce
lulu <lukaspoggemann@gmail.com>
2025-03-02 23:18:32 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ echo \
 "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
 $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+apt-get install ufw fail2ban unattended-upgrades clamav clamav-daemon docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 dpkg-deb --build k2h
 dpkg -i k2h.deb
 apt-get install --fix-broken -y
