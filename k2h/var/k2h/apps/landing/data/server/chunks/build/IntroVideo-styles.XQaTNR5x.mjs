03150ed8441f6abcd2c5b62579c2f9936243e1ff
lulu <lukaspoggemann@gmail.com>
2025-03-12 18:18:57 +0100

    landing pre update

--- k2h/var/k2h/apps/landing/data/server/chunks/build/IntroVideo-styles.XQaTNR5x.mjs
+++
@@ -1,6 +0,0 @@
-import { I as IntroVideo_vue_vue_type_style_index_0_scoped_2a92cdde_lang } from './IntroVideo-styles-1.mjs--oH-dhG7.mjs';
-
-const IntroVideoStyles_XQaTNR5x = [IntroVideo_vue_vue_type_style_index_0_scoped_2a92cdde_lang, IntroVideo_vue_vue_type_style_index_0_scoped_2a92cdde_lang];
-
-export { IntroVideoStyles_XQaTNR5x as default };
-//# sourceMappingURL=IntroVideo-styles.XQaTNR5x.mjs.map
--- k2h/var/k2h/apps/landing/data/server/chunks/build/LandingCard-styles-1.mjs-Bwb5g-pT.mjs
+++
@@ -1,4 +0,0 @@
-const LandingCard_vue_vue_type_style_index_0_scoped_437fae84_lang = ".background-gradient[data-v-437fae84]:before{background:radial-gradient(250px circle at var(--x) var(--y),var(--a3ecdd52) 0,transparent 100%);will-change:background}.to[data-v-437fae84]:hover{--tw-ring-color:var(--a3ecdd52)}.dark .background-gradient[data-v-437fae84]:before{background:radial-gradient(250px circle at var(--x) var(--y),var(--0ddb8715) 0,transparent 100%)}.dark .to[data-v-437fae84]:hover{--tw-ring-color:var(--0ddb8715)}";
-
-export { LandingCard_vue_vue_type_style_index_0_scoped_437fae84_lang as L };
-//# sourceMappingURL=LandingCard-styles-1.mjs-Bwb5g-pT.mjs.map
