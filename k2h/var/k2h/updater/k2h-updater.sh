ab6b1192c60ad0f533fd48f495c91eb50bd88de1
Lukas Poggemann <lukaspoggemann@gmail.com>
2025-04-21 23:29:38 +0200

    Update k2h-updater.sh

--- k2h/var/k2h/updater/k2h-updater.sh
+++ k2h/var/k2h/updater/k2h-updater.sh
@@ -1,20 +1,42 @@
 #!/bin/bash
 
+# Logdatei
+LOG_FILE="/var/k2h/updater/update.log"
+
+# Loggingfunktion mit Zeitstempel
+log() {
+  echo "[$(date '+%Y-%m-%d %H:%M:%S')] $*" >> "$LOG_FILE"
+}
+
 # Temporäres Arbeitsverzeichnis erstellen
 TMP_DIR=$(mktemp -d)
+log "Temporäres Verzeichnis erstellt: $TMP_DIR"
 
-# In den temporären Ordner wechseln
-cd "$TMP_DIR" || exit 1
+# In das temporäre Verzeichnis wechseln
+cd "$TMP_DIR" || { log "Fehler: Wechsel in $TMP_DIR fehlgeschlagen"; exit 1; }
 
 # Repository klonen
-git clone https://github.com/key2host/lsis || exit 1
+if git clone https://github.com/key2host/lsis >>"$LOG_FILE" 2>&1; then
+  log "Repository erfolgreich geklont."
+else
+  log "Fehler: Klonen des Repositories fehlgeschlagen."
+  exit 1
+fi
 
 # In das Repository wechseln
-cd lsis || exit 1
+cd lsis || { log "Fehler: Verzeichnis 'lsis' nicht gefunden"; exit 1; }
 
 # update.sh ausführen
-bash update.sh || exit 1
+if bash update.sh >>"$LOG_FILE" 2>&1; then
+  log "update.sh erfolgreich ausgeführt."
+else
+  log "Fehler: update.sh fehlgeschlagen."
+  exit 1
+fi
 
-# Zurück und temporären Ordner löschen
+# Zurückwechseln und temporäres Verzeichnis löschen
 cd /
 rm -rf "$TMP_DIR"
+log "Temporäres Verzeichnis gelöscht: $TMP_DIR"
+
+log "Updatevorgang abgeschlossen."
