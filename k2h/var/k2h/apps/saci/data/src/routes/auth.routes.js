b57e47611d190238c29df21b45d8137fcf47de9f
Lukas Poggemann <lukaspoggemann@gmail.com>
2025-04-29 23:03:19 +0200

    Update auth.routes.js

--- k2h/var/k2h/apps/saci/data/src/routes/auth.routes.js
+++ k2h/var/k2h/apps/saci/data/src/routes/auth.routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { authenticateUser } = require('../middlewares/auth.middleware');
-const { login, signup, recover, logout, checkID } = require('../controllers/auth.controller');
+const { login, signup, recover, logout, checkID, refresh } = require('../controllers/auth.controller');
 
 const router = express.Router();
 
 // Login-Route für Authentifizierung (JWT-Token)
 router.post('/login', login);
+router.post('/refresh', refresh);
 router.post('/signup', signup);
 router.post('/recover', recover);
 router.post('/checkid', authenticateUser, checkID)
